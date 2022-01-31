2fc8b5a0cc784617b7a9f6db964982394ac0a66d
IsaqueP <isaquepinheiro2002@gmail.com>
2022-01-31 19:28:34 -0300

    Segunda aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import { Button } from './components/button'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
+ 
+
   return (
-    
-    <div>
-      <Button />
-    </div>
-
-
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms/new" element={<NewRoom />} />        
+        </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
-
+ 
 export default App;
